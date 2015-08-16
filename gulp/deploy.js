a442f619d69ae40e46b69da8e13cea72815d04be
Paulo Luan <pauloluan.inova@gmail.com>
2015-08-15 21:11:03 -0300

    added gulp script to deploy to gh-pages

---
+++ gulp/deploy.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe($.ghPages({
+            force: true
+        }));
+});
